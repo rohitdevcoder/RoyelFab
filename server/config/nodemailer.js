7527596778565b9f5816ac1d74bff9562cd1897f
rohitdevcoder <rohitdevlink98@gmail.com>
2025-10-12 10:59:56 +0530

    node mailer edit

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -4,16 +4,16 @@ import nodemailer from 'nodemailer'
 // For example: process.env.EMAIL_USER and process.env.EMAIL_PASS
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com', // Replace with your SMTP server host (e.g., 'smtp.gmail.com')
-    port: 587,
-    secure: false, // true for port 465, false for other ports
+    port: 465,
+    secure: true, // true for port 465, false for other ports
     auth: {
         user: process.env.USER_EMAIL, // Replace with your email address
         pass: process.env.USER_PASSWORD // Replace with your email password or an app-specific password
     },
     // Add this to prevent errors on local development
-    tls: {
-        rejectUnauthorized: false
-    }
+    // tls: {
+    //     rejectUnauthorized: false
+    // }
 });
 
 // Verify the connection configuration
